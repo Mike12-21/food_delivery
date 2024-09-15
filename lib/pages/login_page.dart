66db4b25548cd35d61134c1d5a94b554a17e98c4
Michael Ashenafi <sebmike1227@gmail.com>
2024-09-16 01:37:01 +0300

    Login page and registerpage

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:food_delivery/components/my_button.dart';
+import 'package:food_delivery/components/my_textfield.dart';
+
+class LoginPage extends StatefulWidget {
+  final void Function()? onTap;
+
+  const LoginPage({
+    super.key,
+    required this.onTap,
+  });
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController emailcontroller = TextEditingController();
+  final TextEditingController passwordcontroller = TextEditingController();
+
+  void login() {
+    // Navigator.push(context, MaterialPageRoute(builder: (context)=> HomePage(),))
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.delivery_dining_rounded,
+              size: 100,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+            const SizedBox(height: 25),
+            Text(
+              'Kushna Food Delivery',
+              style: TextStyle(
+                fontSize: 16,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+            ),
+            const SizedBox(height: 25),
+            MyTextField(
+              controller: emailcontroller,
+              hintText: 'Email',
+              obsecureText: false,
+            ),
+            const SizedBox(height: 10),
+            MyTextField(
+              controller: passwordcontroller,
+              hintText: 'Password',
+              obsecureText: true,
+            ),
+            const SizedBox(height: 10),
+            MyButton(
+              text: 'Sign in',
+              onTap: () {},
+            ),
+            const SizedBox(height: 25),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Not a member',
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                  ),
+                ),
+                const SizedBox(
+                  width: 4,
+                ),
+                GestureDetector(
+                  onTap: widget.onTap,
+                  child: Text(
+                    'Register now',
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.inversePrimary,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/register_page.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+import '../components/my_button.dart';
+import '../components/my_textfield.dart';
+
+class RegisterPage extends StatefulWidget {
+  final void Function()? onTap;
+  const RegisterPage({
+    super.key,
+    required this.onTap,
+  });
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final TextEditingController emailcontroller = TextEditingController();
+  final TextEditingController passwordcontroller = TextEditingController();
+  final TextEditingController confirmpasswordcontroller =
+      TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.lock_open_rounded,
+              size: 100,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+            const SizedBox(height: 25),
+            Text(
+              'Create an account',
+              style: TextStyle(
+                fontSize: 16,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+            ),
+            const SizedBox(height: 25),
+            MyTextField(
+              controller: emailcontroller,
+              hintText: 'Email',
+              obsecureText: false,
+            ),
+            const SizedBox(height: 10),
+            MyTextField(
+              controller: passwordcontroller,
+              hintText: 'Password',
+              obsecureText: true,
+            ),
+            const SizedBox(height: 10),
+            MyTextField(
+              controller: confirmpasswordcontroller,
+              hintText: 'Confirm password',
+              obsecureText: true,
+            ),
+            const SizedBox(height: 10),
+            MyButton(
+              text: 'Sign up',
+              onTap: () {},
+            ),
+            const SizedBox(height: 25),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Already have an account?',
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                  ),
+                ),
+                const SizedBox(
+                  width: 4,
+                ),
+                GestureDetector(
+                  onTap: widget.onTap,
+                  child: Text(
+                    'Login ',
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.inversePrimary,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
